3f291d1c34a5abe06a6417f6d12921eb91711375
junior-slv <dirceusilva0422@gmail.com>
2022-11-08 17:18:45 -0300

    style: Move pages

---
+++ src/AppCss.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+display: flex;
+
+`
+export const LeftMenu = styled.div`
+width: 100vw;
+height: 100vh;
+background #fff;
+display: flex;
+
+`
+export const RightMenu = styled.div`
+width: 100vw;
+height: 100vh;
+background: #000;
+display: flex;
+
+`
+export const Card = styled.div`
+
+`
---
+++ src/components/StyledButton.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+padding: 16px 32px;
+background: #fff;
+´
