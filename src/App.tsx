be0561482cc09d6960e294c8b24f5bfed6dbbfe1
Junior <dirceusilva0422@gmail.com>
2022-11-12 04:40:23 -0300

    stlye: modified styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { useState } from 'react';
-import { StyledContainer, StyledButton, CardBackground, LeftMenu, RightMenu, Card, StyledTitle, StyledImg} from './components/StyledComponents'
+import { Container, 
+  StyledButton, CardBackground,
+  LeftMenu, RightMenu, 
+  Card, P, Infos, GlobalStyle,
+  AvatarIMG, Iconsvg} 
+  from './components/StyledComponents'
 import axios from "axios";
 import followersicon from './assets/followers.svg';
 import followingicon from './assets/following.svg';
@@ -9,28 +14,27 @@ import repositoryicon from './assets/repository.svg';
 type GITHUBResponse = {
   name: string;
   avatar_url: string;
-  bio: string;
   following: string;
   followers: string;
   public_repos: string;
   location: string;
 };
 function App() {
+
   const [userName, setUserName] = useState("");
   const [name, setName] = useState("Aguardando...");
-  const [bio, setBio] = useState("Aguardando...");
   const [avatarURL, setAvatarURL] = useState("Aguardando...");
   const [following, setFollowing] = useState("Aguardando...");
   const [followers, setFollowers] = useState("Aguardando...");
   const [publicrepos, setPublicrepos] = useState("Aguardando...");
   const [location, setlocation] = useState("Aguardando...");
 
+  
   const handleSearch = () => {
     axios
       .get<GITHUBResponse>(`https://api.github.com/users/${userName}`)
       .then((res) => {
         setName(res.data.name);
-        setBio(res.data.bio);
         setAvatarURL(res.data.avatar_url);
         setFollowing(res.data.following);
         setFollowers(res.data.followers);
@@ -43,34 +47,31 @@ function App() {
   };
   
   return (
-    
-    <StyledContainer>  
+    <><GlobalStyle />
+    <Container>
       <LeftMenu>
         <CardBackground>
           <Card>
-            <StyledTitle>{name}</StyledTitle>
-            <img id='useravatar'src={avatarURL}/>
-            <div className='infos'>
-              <p><img src={followersicon}/> {followers} Seguidores</p>
-              <p><img src={followingicon}/> {following} Seguindo</p>
-              <p><img src={repositoryicon}/> {publicrepos} Repositórios</p>
-              <p><img src={locationicon}/> {location}</p>
-            </div>   
+            <P>{name}</P>
+            <AvatarIMG  src={avatarURL} />
+            <Infos>
+              <P><Iconsvg src={followersicon} alt='icon'/> {followers} Seguidores</P>
+              <P><Iconsvg src={followingicon} alt='icon'/> {following} Seguindo</P>
+              <P><Iconsvg src={repositoryicon} alt='icon'/> {publicrepos} Repositórios</P>
+              <P><Iconsvg src={locationicon} alt='icon'/> {location}</P>
+            </Infos>
           </Card>
         </CardBackground>
       </LeftMenu>
       <RightMenu>
-
-      <input
-              type="text"
-              placeholder="Digite um username"
-              onChange={(e) => setUserName(e.target.value)}
-            />
-            <button onClick={handleSearch}>Buscar</button>
-
-        <StyledButton> Gerar background </StyledButton>
+        <P>Insira o usuário do Github</P>
+        <input
+          type="text"
+          placeholder="Digite um username"
+          onChange={(e) => setUserName(e.target.value)} />
+       <StyledButton onClick={handleSearch}>Buscar</StyledButton>
       </RightMenu>
-  </StyledContainer>
+    </Container></>
   
   )
   ;
--- src/components/StyledComponents.tsx
+++ src/components/StyledComponents.tsx
@@ -1,40 +1,51 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 
+export const GlobalStyle = createGlobalStyle`
+body{
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+}
+` 
 export const StyledButton = styled.button`
     padding: 16px 32px;
     background: #000;
     border-radius: 15px;
     color: #fff;
 ` 
-export const StyledContainer = styled.div`
+export const Container = styled.div`
     margin: 0;
     display: flex;
 
 `
 export const LeftMenu = styled.div`
-width: 100vw;
-height: 100vh;
-background #000000;
-display: flex;
-align-items: center;
-justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background: #3B3486;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 export const RightMenu = styled.div`
-width: 100vw;
-height: 100vh;
-background: #000000;
-display: flex;
-align-items: center;
-
+    width: 100vw;
+    height: 100vh;
+    background: #3B3486;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction:column;
 
 `
 export const CardBackground = styled.div`
     width: 50%;
     height: 85%;
     border-radius: 45px;
-    background: #fa00e5;
+    -webkit-box-shadow: 0px 0px 42px -1px rgba(0,0,0,0.75);
+    -moz-box-shadow: 0px 0px 42px -1px rgba(0,0,0,0.75);
+    box-shadow: 0px 0px 42px -1px rgba(0,0,0,0.75);
+    background: #7DE5ED;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -43,18 +54,35 @@ export const Card = styled.div`
     width: 80%;
     height: 90%;
     border-radius: 45px;
-    background #0E1218;
+    border: 1px solid #000;
+    background #3B3486;
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction:column;
 `
-export const StyledTitle = styled.p`
+export const P = styled.p`
     color: #fff;
 `
-export const StyledImg = styled.img`
-    width: 50px;
-    height: 50px;
-`
+export const Iconsvg = styled.img`
 
+`
+export const AvatarIMG = styled.img`
+    width: 200px;
+    height: 200px;
+    border-radius: 15px;
+    border: 2px solid #7DE5ED;
+    overflow: hidden;
+`
+export const Infos = styled.div`
+    color: #fff;
+    background-color: rgba(0,0,0, 0.6);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction:column;
+    border-radius: 15px;
+    width: 212px;
+    height: 202px;
+`
 
